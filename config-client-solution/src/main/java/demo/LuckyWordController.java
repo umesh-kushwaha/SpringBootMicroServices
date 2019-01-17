c15d40293f47f5619762d216bcc38caf125af16b
umesh-kushwaha <umesh220388@gmail.com>
2019-01-17 22:49:44 +0530

    config client added

--- config-client-solution/src/main/java/demo/LuckyWordController.java
+++ config-client-solution/src/main/java/demo/LuckyWordController.java
@@ -1,15 +1,15 @@
-package com.microservice.client.service;
+package demo;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-	
+public class LuckyWordController {
+	 
 	@Value("${lucky-word}") String luckyWord;
-
-	@RequestMapping("/lucky-word")
+	  
+	  @GetMapping("/lucky-word")
 	  public String showLuckyWord() {
 	    return "The lucky word is: " + luckyWord;
 	  }
